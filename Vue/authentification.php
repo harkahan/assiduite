3fb8f18fc888a106ef3f5fdfb3972f307d4c65b5
E164708F <nostale789>
2017-11-22 17:20:34 +0100

    Plein de beau changement

---
+++ Vue/authentification.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<!--Version 1.00-->
+
+<head>
+    <title>Connexion au service </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../bootstrap-4.0.0-beta.2-dist/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+
+<body class="text-center">
+    <h1>Assiduité</h1>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="depot.html">Dépôt</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="consultation.html">Consultation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="gestion.html">Gestion</a>
+            </li>
+        </ul>
+    </nav>
+    <h2>Connexion au service</h2>
+    <form action="../Controleur/Authentification.php" method="post">
+        Identifiant :
+        <input type="text" name="identifiant" value=""><br> Mot de passe :
+    <input type="password" name="pass" value=""><br>
+<input class="form-check-input" type="checkbox"> Se souvenir de moi</br>
+<input class="btn btn-outline-primary" type="submit" value="Connexion">
+</form>
+</body>
+
+</html>
---
+++ Vue/gestion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<!--Version 1.00-->
+
+<head>
+    <title>Absence</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../bootstrap-4.0.0-beta.2-dist/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+
+<body class="text-center">
+    <h1>Assiduité</h1>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="../index.html">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="depot.html">Dépôt</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="consultation.html">Consultation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="gestion.html">Gestion</a>
+            </li>
+        </ul>
+    </nav>
+    <h2>Gestion des absences</h2>
+</body>
+
+</html>
