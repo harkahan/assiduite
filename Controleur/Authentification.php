f501eb193583cbecd2f1469929259eea33aa6b11
ebacala <e.bacala@gmail.com>
2017-11-22 11:48:45 +0100

    .

--- Controleur/Authentification.php
+++ Controleur/Authentification.php
@@ -7,7 +7,7 @@ if ($auth->verifieAuthentification($_POST["identifiant"], $_POST["pass"])) {
   header("Location: ../Vue/depot.php");
 }
 else{
-  echo "<h1> too bad</h1>";
+  echo "<h1>Connexion échouée</h1>";
   $_POST = null;
 }
 
