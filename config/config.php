7cf1b7cef10df4d1104f4d78c0181170ab7a4313
E164786Q <evann.bacala@etu.univ-nantes.fr>
2017-11-28 13:46:38 +0100

    .

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+  define("HOME_SITE", __DIR__ . "/..");
+
+  define("PATH_VUE", HOME_SITE . "/Vue");
+  define("PATH_CONTROLEUR", HOME_SITE . "/Controleur");
+  define("PATH_MODELE", HOME_SITE . "/Modele");
+  define("PATH_BASESQL", HOME_SITE . "/BaseSQL");
+
+?>
