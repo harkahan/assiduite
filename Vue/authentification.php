fb53041925652712920fd5b65ccae95748af5dc3
E164786Q <evann.bacala@etu.univ-nantes.fr>
2017-11-28 13:51:38 +0100

    .

--- Vue/authentification.php
+++ Vue/authentification.php
@@ -5,36 +5,34 @@
 <head>
     <title>Connexion au service</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../bootstrap-4.0.0-beta.2-dist/css/bootstrap.min.css" crossorigin="anonymous">
 </head>
 
-<body class="text-center">
+<body>
     <h1>Assiduité</h1>
-    <nav class="navbar navbar-expand-sm bg-light navbar-light">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="../index.html">Accueil</a>
+    <nav>
+        <ul>
+            <li>
+                <a href="../index.html">Accueil</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="depot.php">Dépôt</a>
+            <li>
+                <a href="depot.php">Dépôt</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="consultation.php">Consultation</a>
+            <li>
+                <a href="consultation.php">Consultation</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="gestion.php">Gestion</a>
+            <li>
+                <a href="gestion.php">Gestion</a>
             </li>
         </ul>
     </nav>
     <h2>Connexion au service</h2>
     <form action="../Controleur/Authentification.php" method="post">
         Identifiant :
-        <input type="text" name="identifiant" value=""><br> Mot de passe :
-    <input type="password" name="pass" value=""><br>
-<input class="form-check-input" type="checkbox"> Se souvenir de moi</br>
-<input class="btn btn-outline-primary" type="submit" value="Connexion">
-</form>
+        <input type="text" name="identifiant" value=""><br />
+        Mot de passe :
+        <input type="password" name="pass" value=""><br />
+        <input type="submit" value="Connexion">
+    </form>
 </body>
 
 </html>
